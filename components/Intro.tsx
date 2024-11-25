0c6f7cb27258d715411bd4ea08edf078ee8039e1
oramatel <95945776+oramatel@users.noreply.github.com>
2024-11-25 21:05:05 +1100

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,22 +1,21 @@
 import Image from "next/image";
-import ProfilePic from "@/app/images/yatharth.png";
+import ProfilePic from "@/app/images/oramatel.png";
 
 export default function Intro() {
   return (
     <div className="w-full flex flex-col-reverse lg:flex-row gap-14 justify-between items-center -z-10">
       <div className="flex flex-col gap-2 w-full lg:w-4/5">
-        <h1 className="text-4xl font-bold mb-4">Hi! I&apos;m Yatharth.</h1>
+        <h1 className="text-4xl font-bold mb-4">Hello! I&apos;m David.</h1>
         <p className="text-base text-gray-800 dark:text-gray-300">
-          Seasoned Fullstack Engineer with 4+ years in startups. I excel in
-          building applications from scratch to production-ready. My expertise
-          spans across frontend, backend, and full-stack development, with a
-          knack for system design and scalability.
+          With over 20 years of experience, I am an accomplished, reliable, and dedicated Network and Technology Solutions Engineer.
+          My track record is defined by consistently delivering secure and resilient systems that align with business objectives, on time and within budget.
+          TBC
         </p>
       </div>
       <div className="flex items-center justify-center md:mb-0">
         <Image
           src={ProfilePic}
-          alt="Yatharth"
+          alt="David"
           width={160}
           height={160}
           className="border-2 border-gray-100 rounded-full object-cover"
